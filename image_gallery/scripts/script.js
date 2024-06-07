3b90212ee58884cfc9b43b37860b3808c984fb26
habibundayishimiye <habibundayishimiye@gmail.com>
2024-06-07 18:51:15 +0200

    My first change on Image-gallery
    My first change on Image-gallery

---
+++ image_gallery/scripts/script.js
@@ -0,0 +1,13 @@
+// // Get all elements with the class "image-container"
+// const imageContainers = document.querySelectorAll('.image-container');
+
+// // Loop through each image container and add mouseover and mouseout event listeners
+// imageContainers.forEach(container => {
+//     container.addEventListener('mouseover', function() {
+//         this.classList.add('hovered');
+//     });
+
+//     container.addEventListener('mouseout', function() {
+//         this.classList.remove('hovered');
+//     });
+// });
